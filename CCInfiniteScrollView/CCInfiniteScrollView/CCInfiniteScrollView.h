e8545a3b1b4b3bd2bb6fcef58ac3f2680311a3c0
cc <chenlove523@163.com>
2016-04-21 09:21:47 +0800

    修改

--- CCInfiniteScrollView/CCInfiniteScrollView/CCInfiniteScrollView.h
+++ CCInfiniteScrollView/CCInfiniteScrollView/CCInfiniteScrollView.h
@@ -3,7 +3,7 @@
 //  CCInfiniteScrollView
 //
 //  Created by cc on 16/4/7.
-//  Copyright © 2016年 cc. All rights reserved.
+//  Copyright © 2016年 cochn. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- CCInfiniteScrollView/ViewController.m
+++ CCInfiniteScrollView/ViewController.m
@@ -3,7 +3,7 @@
 //  CCInfiniteScrollView
 //
 //  Created by cc on 16/4/7.
-//  Copyright © 2016年 yggx. All rights reserved.
+//  Copyright © 2016年 cochn. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -22,11 +22,10 @@
 
 - (void)scrollView{
     
-    // 情景三：图片配文字
-    NSArray *titles = @[@"新建交流QQ群：185534916 ",
+    NSArray *titles = @[@"这是一个文字上下轮播图",
                         @"感谢您的支持，如果下载的",
                         @"如果代码在使用过程中出现问题",
-                        @"您可以发邮件到gsdios@126.com"
+                        @"您可以发邮件到chenlove523@163.com"
                         ];
 
     CCInfiniteScrollView *scrollView = [CCInfiniteScrollView cycleScrollViewWithFrame:CGRectMake(50, 100, self.view.frame.size.width-50, 35)];
